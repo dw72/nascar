c1c3535225066a985872c45a558f2237500dfe6e
Dariusz Włodarczyk <milley@krotoszyn.pl>
2019-01-14 20:42:40 +0100

    Removed logging google maps api key to console

--- plugins/vue-googlemaps.js
+++ plugins/vue-googlemaps.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import 'vue-googlemaps/dist/vue-googlemaps.css'
 import VueGoogleMaps from 'vue-googlemaps'
 
-console.log(process.env.GMAPS_API_KEY)
-
 Vue.use(VueGoogleMaps, {
   load: {
     apiKey: process.env.GMAPS_API_KEY
