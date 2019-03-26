bd2da35cb1b93a8648e2e099c6aeb9f120a65a6d
Dariusz Włodarczyk <milley@krotoszyn.pl>
2019-03-26 20:13:41 +0100

    Better error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Strona główna</nuxt-link>
+  <div class="page">
+    <div class="number">{{ error.statusCode }}</div>
+    <h2 class="message">Ops... coś poszło nie tak!</h2>
+    <p>{{ error.message }}</p>
+    <button class="button button--primary button--rounded" @click="$router.go(-1)">⬅&nbsp;&nbsp;Wróć</button>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    'error': { type: Object, required: true }
+    error: { type: Object, required: true }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.page {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 45vh;
+  min-height: 400px;
+}
+
+.number {
+  font-size: 4em;
+  font-weight: bold;
+  margin-top: -1em;
+  opacity: 0.25;
+}
+
+h2 {
+  margin: 0;
+  font-size: 1.75em;
+}
+
+p {
+  font-weight: bold;
+  margin: 0 0 2em;
+  opacity: 0.5;
+}
+
+.button {
+  padding: 0.5em 1.5em;
+}
+</style>
