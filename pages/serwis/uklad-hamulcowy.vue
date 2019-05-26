12546cfa5b039883b67176a35081e39e6c5bde73
Dariusz Włodarczyk <milley@krotoszyn.pl>
2019-05-26 18:57:39 +0200

    Changed Breaks service page

--- pages/serwis/uklad-hamulcowy.vue
+++ pages/serwis/uklad-hamulcowy.vue
@@ -8,12 +8,6 @@
         a tempo ich zużycia zależy także od stylu jazdy prowadzącego."
     >
       <flex-list item-width="1/2">
-        <icon-card title="Bilans hamulców" icon="check" icon-size="md" :bordered="false" horizontal>
-          <p>
-            nasi fachowcy sprawdzą stan zużycia klocków hamulcowych, jakość płynu hamulcowego oraz ślady nieprawidłowego zużycia opon.
-            Dzięki regularnej kontroli będziesz mieć pewność, że niezależnie od warunków pogodowych oraz sposobu eksploatacji auta hamulce Cię nie zawiodą.
-          </p>
-        </icon-card>
         <icon-card
           title="Diagnostyka układu hamulcowego"
           icon="check"
@@ -22,9 +16,21 @@
           horizontal
         >
           <p>
-            Regularna kontrola związana z oceną sprawności układu hamulcowego to jedna z najważniejszych czynności związanych z eksploatacją auta.
-            Serwis powinien obejmować przede wszystkim pomiar grubości okładzin i tarcz hamulcowych, pomiar bicia bocznego tarcz,
-            sprawdzenie stanu przewodów hamulcowych pod kątem zużycia i ewentualnych wycieków oraz pomiar jakości płynu hamulcowego.
+            Nasi fachowcy sprawdzą stan zużycia klocków hamulcowych, jakość płynu hamulcowego oraz ślady nieprawidłowego zużycia opon.
+            Serwis obejmuje także pomiar grubości okładzin i tarcz hamulcowych, pomiar bicia bocznego tarcz,
+            sprawdzenie stanu przewodów hamulcowych oraz pomiar jakości płynu hamulcowego.
+          </p>
+        </icon-card>
+        <icon-card
+          title="Wymiana przewodów i zacisków hamulcowych"
+          icon="check"
+          icon-size="md"
+          :bordered="false"
+          horizontal
+        >
+          <p>
+            Przewody, zaciski hamulcowe oraz cylinderki to elementy układu, które w równym stopniu co pozostałe elementy mają wpływ na skuteczność hamowania.
+            W naszym serwisie możesz je naprawić lub wymienić.
           </p>
         </icon-card>
         <icon-card
@@ -57,25 +63,12 @@
             Doradcy serwisowi dobiorą właściwy płyn hamulcowy, zależnie od konstrukcji samochodu oraz zastosowania dodatkowych układów, takich jak ABS lub ESP.
           </p>
         </icon-card>
-        <icon-card
-          class="full-width"
-          title="Wymiana przewodów i zacisków hamulcowych"
-          icon="check"
-          icon-size="md"
-          :bordered="false"
-          horizontal
-        >
-          <p>
-            Przewody, zaciski hamulcowe oraz cylinderki to elementy układu, które w równym stopniu co pozostałe elementy mają wpływ na skuteczność hamowania.
-            W naszym serwisie możesz je naprawić lub wymienić.
-          </p>
-        </icon-card>
       </flex-list>
     </page-section>
     <page-section header="removed" full-width grey>
       <p class="centered bigger">
-        Nowoczesny sprzęt jakim dysponujemy w naszych serwisach pozwala na szybkie i precyzyjne wykrycie usterek,
-        a każda usługa wykonywana jest zgodnie z wytycznymi producentów pojazdów i układów hamulcowych.
+        Regularna kontrola związana z oceną sprawności układu hamulcowego
+        <br>to jedna z najważniejszych czynności związanych z eksploatacją auta.
       </p>
     </page-section>
   </main>
@@ -90,9 +83,3 @@ export default {
   components: { PageSection, FlexList, IconCard }
 }
 </script>
-
-<style lang="scss" scoped>
-.flex-list > .full-width {
-  flex-basis: 100%;
-}
-</style>
