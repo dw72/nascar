84bd489c0e0be7ebc91ff239a1546834d51abc6c
dw72 <milley@krotoszyn.pl>
2022-03-12 16:05:16 +0100

    Embeded new reservation app

--- pages/termin.vue
+++ pages/termin.vue
@@ -3,25 +3,40 @@
     <page-section
       class="timeblockr-section"
       title="Zarezerwuj termin"
-      description="Użyj poniższego formularza aby wybrać usługi a następnie umówić wizytę w naszym warsztacie w dogodnym dla Ciebie terminie."
+      description="Użyj poniższej aplikacji aby umówić wizytę w naszym warsztacie w dogodnym dla Ciebie terminie."
       full-width
     >
-      <timeblockr />
+      <div>
+        <iframe id="calendar" ref="calendar" :src="source" />
+      </div>
+      <div>
+        <p class="disclaimer">
+          * Dla Twojej wygody osadziliśmy zewnętrzną aplikację do rezerwacji na
+          naszej stronie.
+        </p>
+      </div>
     </page-section>
   </main>
 </template>
 
 <script>
 import PageSection from '@/components/PageSection'
-import Timeblockr from '@/components/Timeblockr'
 
 export default {
-  components: { PageSection, Timeblockr },
+  components: { PageSection },
+  data() {
+    return {
+      source: 'https://www.bookingforsure.eu/bc?servis=4143&lng=pl',
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-.timeblockr-section {
-  padding-bottom: 1.5em;
+#calendar {
+  border: none;
+  box-sizing: border-box;
+  min-height: 70vh;
+  width: 100%;
 }
 </style>
