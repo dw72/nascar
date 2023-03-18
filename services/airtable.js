0d890c120eb6488077cae968049b82ac6cc47390
dw72 <milley@krotoszyn.pl>
2023-03-18 20:19:10 +0100

    Refactor airtable code

--- services/airtable.js
+++ services/airtable.js
@@ -1,5 +1,9 @@
 import Airtable from 'airtable'
 
-const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID)
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+const base = new Airtable.base(process.env.AIRTABLE_BASE_ID)
 
-export default airtable
+export default base
